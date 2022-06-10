7566d462024334a3221f8b00cc92909ea9d801af
Yurij <yuran.ignatenko@yandex.ru>
2022-06-10 10:53:50 +0300

    создан пакеты и примеры использования

---
+++ CtrlCmd.go
@@ -0,0 +1,79 @@
+package CtrlCmd
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+// alternative fmt.Sprintf
+var F = fmt.Sprintf
+
+// custom symbols
+var (
+	Block     = "█"
+	TriangleL = "▶"
+	TriangleR = "◀"
+	TriangleU = "▲"
+	TriangleD = "▼"
+)
+
+// convert  user params in eq cmd args
+func ColorMode(c string, mode string) (int, string) {
+	rcolor := -1
+	rmode := "ab"
+	switch c {
+	case "black":
+		rcolor = 0
+	case "red":
+		rcolor = 1
+	case "green":
+		rcolor = 2
+	case "yellow":
+		rcolor = 3
+	case "blue":
+		rcolor = 4
+	case "violet":
+		rcolor = 5
+	case "cyan":
+		rcolor = 6
+	case "white":
+		rcolor = 7
+	}
+
+	switch mode {
+	case "bg":
+		rmode = "ab"
+	case "fg":
+		rmode = "af"
+	}
+	return rcolor, rmode
+}
+
+// print color command line
+func LineColor(line string, color string, mode string) {
+	c, m := ColorMode(color, mode)
+	re := exec.Command("bash", "-c", F("echo `tput  set%v %v`\"%+v\"`tput sgr0`", m, c, line))
+	res, _ := re.Output()
+	fmt.Print(string(res))
+}
+
+// print format command line
+func LineF(line string) {
+	re := exec.Command("bash", "-c", F("%v", line))
+	res, _ := re.Output()
+	fmt.Print(string(res))
+}
+
+// print easy command line
+func Line(line string) {
+	re := exec.Command("bash", "-c", line)
+	res, _ := re.Output()
+	fmt.Print(string(res))
+}
+
+// get result command line
+func LineGet(line string) string {
+	re := exec.Command("bash", "-c", line)
+	res, _ := re.Output()
+	return string(res)
+}
---
+++ _example/example.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	. "CtrlCmd"
+	"time"
+)
+
+func main() {
+	i := 0
+	for {
+		Line("clear")
+		i++
+		LineColor("hello", "red", "bg")
+		LineColor("hello", "green", "bg")
+		LineColor("hello", "blue", "bg")
+		LineColor("hello", "yellow", "bg")
+		LineColor("hello", "cyan", "bg")
+		LineColor("hello", "black", "bg")
+		LineColor("hello", "white", "bg")
+		LineColor("hello", "violet", "bg")
+
+		LineColor("hello", "red", "fg")
+		LineColor("hello", "green", "fg")
+		LineColor("hello", "blue", "fg")
+		LineColor("hello", "yellow", "fg")
+		LineColor("hello", "cyan", "fg")
+		LineColor("hello", "black", "fg")
+		LineColor("hello", "white", "fg")
+		LineColor("hello", "violet", "fg")
+
+		Line("echo `tput setab 1`Hello`tput sgr0`")
+		Line("echo `tput setab 2`Hello`tput sgr0`")
+		Line("echo `tput setaf 3``tput setab 5`Hello`tput sgr0`")
+
+		Line(F("echo \"%v\"", Block))
+		Line(F("echo \"%v\"", TriangleL))
+		Line(F("echo \"%v\"", TriangleR))
+		Line(F("echo \"%v\"", TriangleU))
+		Line(F("echo \"%v\"", TriangleD))
+
+		pid := LineGet("ps aux | grep main | grep -v color")
+
+		Line(F("echo [isec:%v] -- [pid:%v]", i, pid))
+		time.Sleep(time.Second * 1)
+	}
+}
